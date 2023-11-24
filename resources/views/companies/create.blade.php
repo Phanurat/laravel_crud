1e9b1b1e3c5be465ca061e369e46e7b077af1a9d
Phanurat <paunavt5@gmail.com>
2023-11-24 10:10:48 +0700

    Add Form

---
+++ resources/views/companies/create.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Create Data</title>
+</head>
+<body>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2>Add Company</h2>
+            </div>
+            <div>
+                <a href="{{route('companies.index')}}" class="btn btn-primary">Bank</a>
+            </div>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <form action="{{ route('companies.store') }} " method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <strong>Company Name</strong>
+                            <input type="text" name="name" class="form-control" placeholder="Company Name">
+                            @error('name')
+                                <div class="alert alert-danger"> {{ $message }} </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <strong>Company Email</strong>
+                            <input type="email" name="email" class="form-control" placeholder="Company Email">
+                            @error('email')
+                                <div class="alert alert-danger"> {{ $message }} </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <strong>Company Address</strong>
+                            <input type="text" name="address" class="form-control" placeholder="Company Address">
+                            @error('address')
+                                <div class="alert alert-danger"> {{ $message }} </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>                
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
