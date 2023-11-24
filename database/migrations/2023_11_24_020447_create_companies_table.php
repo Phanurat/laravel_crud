87f09e0d8cb8d9b1e942bececb4407f11b88a125
Phanurat <paunavt5@gmail.com>
2023-11-24 09:10:53 +0700

    Create Controller

--- database/migrations/2023_11_24_020447_create_companies_table.php
+++ database/migrations/2023_11_24_020447_create_companies_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
             $table->timestamps();
         });
     }
